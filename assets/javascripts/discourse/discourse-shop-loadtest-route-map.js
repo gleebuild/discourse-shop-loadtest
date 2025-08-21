69a7b4c456ebdae54b83858a78d8eae660cb0460
gleebuild <gleebuild@gmail.com>
2025-08-21 17:20:07 +0000

    Delete assets directory

--- assets/javascripts/discourse/discourse-shop-loadtest-route-map.js
+++
@@ -1,6 +0,0 @@
-
-export default function () {
-  this.route("shoptest", { path: "/shop" });
-  this.route("shoptest-admin", { path: "/shop/admin" });
-  this.route("adminPlugins.shoptest", { path: "/admin/plugins/shop" });
-}
